8a6ba551de264f5e5c51eac52a98398c90e5c99b
Ritesh Kumar <ritesh.kumar@progcap.com>
2024-08-17 23:36:39 +0530

    know about react

---
+++ Dev_Part1/react_basics/src/1IntroToReact.js
@@ -0,0 +1,50 @@
+//React is a open source javascript library for building user interfaces.
+
+//React is not a framework ,it is a javascript library.
+
+//React does not foces on other aspects of your application like routing or http requests.
+
+//React is only responsible for building reach user interfaces.
+
+//Then how will be handled Routing http requests?
+
+//So don't have worry u about that because of React has Rich ecosystem
+// and palce well with other libraries and it is more than capable for full flexed web applications.
+
+//React is created an d maintained by Facdebook.
+
+//React has component based architecture.
+//Means it will breakdown your application into small encapuslated parts,
+//which can be make composed more complexed ui.
+
+// A traditional website has:->
+// Header
+// SideNav
+// Main Content
+// Footer
+
+//React has reusable code features.
+//means it will reuse component or code by provideing code 
+
+//React is declarative.
+//means Tell React what you want and React will build the actual ui.
+
+//React will handle eddicinently updating and rendering of the components.
+
+//DOM updates are handles gracefully in React.
+
+//Seamlessly integrate react into any of your application.
+
+//React will be fit in portion of your page,or a complete page or even an entire application itself.
+
+//Prerequisities of React:->
+//Html,Css  and javascript fundamentals.
+//ES6
+//javascript- 'this' keyword,filter ,map and reduce.
+//ES6- let and const ,arrow functions,template literals,default parameters,
+//object literals,rest and spread operators and destructuring assignmnets.
+
+//
+
+
+
