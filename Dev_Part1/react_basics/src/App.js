c2543fac1cb0046a77e08dee48d9eab665c214c2
Ritesh Kumar <ritesh.kumar@progcap.com>
2024-08-18 11:46:08 +0530

    Functional component

---
+++ Dev_Part1/react_basics/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from './components/04ReactFunctionalComponent';
+
+function Ritesh() {
+  return (
+    <div className="App">
+        <App/>
+    </div>
+  );
+}
+
+export default Ritesh;
--- Dev_Part1/react_basics/src/index.js
+++ Dev_Part1/react_basics/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './00Intro';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
