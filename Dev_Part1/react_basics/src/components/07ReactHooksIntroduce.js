1f89bbc67e2726bf6cb6dd6fba4ae753e467f61c
Ritesh Kumar <ritesh.kumar@progcap.com>
2024-08-18 12:06:39 +0530

    Little intro of Hooks

---
+++ Dev_Part1/react_basics/src/components/07ReactHooksIntroduce.js
@@ -0,0 +1,10 @@
+//Hooks are a new feature proposal that lets you use state 
+//and other React features without writing a class.
+
+//Role of Hooks:->
+//No breaking changes.
+//Completely opt0in and 100% backward-compatible.
+//Whate ever we are learned so far ,still holds good.
+//Component types-Functional and Class.
+//using state,lifecycle method and 'this' binding.
+//After understanding state ,event binding and lifecycle hooks in class components.
