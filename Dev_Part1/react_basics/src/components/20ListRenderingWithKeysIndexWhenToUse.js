640c8c96a65ee340b5944f92e0045e62e6030ddf
Ritesh Kumar <ritesh.kumar@progcap.com>
2024-09-14 19:10:33 +0530

    rule of using key as index

---
+++ Dev_Part1/react_basics/src/components/20ListRenderingWithKeysIndexWhenToUse.js
@@ -0,0 +1,5 @@
+//Rule of using index as key:->>>
+
+//The items in your list do not have a unique id.
+//The list is a static list and will not change.
+//The list will never be reordered or filtered.
